61856cd936dfd8cd896ce3f5cfda24f6ced13502
Ralph Morton <ralphmorton@gmail.com>
2022-11-14 10:36:25 +0200

    Update

--- src/React/Basic/Native/Internal.js
+++ src/React/Basic/Native/Internal.js
@@ -1,13 +1,13 @@
 const React = require("react")
 const RN = require("react-native")
 
-exports.mergeStyles = function(styles){
-  return Object.assign.apply(null, [{}].concat(styles));
-};
+export const mergeStyles = (styles) => {
+  return Object.assign.apply(null, [{}].concat(styles))
+}
 
-exports.unsafeCreateNativeElement = function(name){
-  return function(props){
-    var children = (props.children) ? props.children : [];
-    return React.createElement.apply(React, [RN[name], props].concat(children));
+export const unsafeCreateNativeElement = (name) => {
+  return (props) => {
+    var children = (props.children) ? props.children : []
+    return React.createElement.apply(React, [RN[name], props].concat(children))
   }
-};
+}
